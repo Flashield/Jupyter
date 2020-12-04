6eab1fd413fd38660cdb38677431d4312d545a9d
zhuangbin <zhuangbin@gmail.com>
2020-12-04 13:45:53 +0800

    2020-12-04 Commit

---
+++ Tmp_Python3.py
@@ -0,0 +1,11 @@
+#%%
+import pandas as pd
+import matplotlib as mpl 
+#%%
+print("Hello, World!")
+pd.DataFrame([[1,2],[3,4],[5,6],[7,8]], columns=['Col1', 'Col2'])
+# %%
+for i in range(10):
+    print(i,end='\t')
+
+# %%
---
+++ zblib/SendSMS.py
@@ -0,0 +1,23 @@
+import cx_Oracle
+import os
+import sys
+
+sms_db = 'esxvm0504'
+sms_db_user = 'ntsms'
+sms_db_password = 'smsinterface'
+
+def sms_send(v_msisdn,v_actid,v_message):
+    sms_conn=cx_Oracle.connect('ntsms','smsinterface','esxvm0504')
+    sms_cur=sms_conn.cursor()    
+    sms_cur.callproc('p_send_sms',[str(v_msisdn),v_actid,v_message])
+    print('Message send to {} : {}'.format(v_msisdn, v_message))
+    sms_cur.close()
+    sms_conn.close()    
+
+if len(sys.argv)==4:
+    sms_msisdn=sys.argv[1]
+    sms_act_id=sys.argv[2]
+    sms_message=sys.argv[3]
+    sms_send(sms_msisdn,sms_act_id,sms_message)
+else:
+    print('Argument Error, Check it!')
